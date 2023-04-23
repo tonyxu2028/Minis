4650b0f4c00fbeaecc7648929e9e4c7450fbf31a
tonyxu2028 <liwei2028@163.com>
2023-04-23 10:19:23 +0800

    ApplicationEvent

--- src/com/minis/beans/factory/config/AbstractAutowireCapableBeanFactory.java
+++ src/com/minis/beans/factory/config/AbstractAutowireCapableBeanFactory.java
@@ -6,39 +6,46 @@ import java.util.List;
 import com.minis.beans.BeansException;
 import com.minis.beans.factory.support.AbstractBeanFactory;
 
-public abstract class AbstractAutowireCapableBeanFactory 
+/**
+ * @author naixixu
+ */
+public abstract class AbstractAutowireCapableBeanFactory
 						extends AbstractBeanFactory implements AutowireCapableBeanFactory{
 	private final List<BeanPostProcessor> beanPostProcessors = new ArrayList<BeanPostProcessor>();
-	
+
+	@Override
 	public void addBeanPostProcessor(BeanPostProcessor beanPostProcessor) {
 		this.beanPostProcessors.remove(beanPostProcessor);
 		this.beanPostProcessors.add(beanPostProcessor);
 	}
+
+	@Override
 	public int getBeanPostProcessorCount() {
 		return this.beanPostProcessors.size();
 	}
+
 	public List<BeanPostProcessor> getBeanPostProcessors() {
 		return this.beanPostProcessors;
 	}
 
+	@Override
 	public Object applyBeanPostProcessorsBeforeInitialization(Object existingBean, String beanName)
 			throws BeansException {
-
 		Object result = existingBean;
 		for (BeanPostProcessor beanProcessor : getBeanPostProcessors()) {
 			beanProcessor.setBeanFactory(this);
 			result = beanProcessor.postProcessBeforeInitialization(result, beanName);
 			if (result == null) {
-				return result;
+				return null;
 			}
 		}
 		existingBean = result;
 		return result;
 	}
 
+	@Override
 	public Object applyBeanPostProcessorsAfterInitialization(Object existingBean, String beanName)
 			throws BeansException {
-
 		Object result = existingBean;
 		for (BeanPostProcessor beanProcessor : getBeanPostProcessors()) {
 			result = beanProcessor.postProcessAfterInitialization(result, beanName);
@@ -47,5 +54,6 @@ public abstract class AbstractAutowireCapableBeanFactory
 			}
 		}
 		return result;
-	}	
+	}
+
 }
